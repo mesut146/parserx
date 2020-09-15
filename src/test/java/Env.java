aa7562e2cb0b0771f7c6c728de6b654b82d97e3a
Mesut <mesutdogansoy@gmail.com>
2020-09-15 14:14:46 +0300

    remove old files

--- src/test/java/Env.java
+++ src/test/java/Env.java
@@ -2,13 +2,11 @@ import java.io.File;
 
 public class Env {
     static String dir;
-    static String testDir;
     static String testJava;
 
     static {
         dir = "/home/mesut/IdeaProjects/parserx";
         //dir = "/storage/emulated/0/AppProjects/parserx";
-        testDir = dir + "/test";
         testJava = dir + "/src/test/java";
     }
 
